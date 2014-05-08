bd9cd282dfa9c79cda3d6f58fbaffc5f784fe094
Bobby Galli <bobbyg603@gmail.com>
2014-05-08 19:41:44 -0400

    modified for heroku

---
+++ node_modules/twit/config1.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key:       'XEVvfWghMoa6EKWGJczqimzq0',
+    consumer_secret:    'IxiyjBxvrT9N22pOiCc3xBiAF3OqKJASatJVsmAJNsyOmy7V3H',
+    access_token:       '1418361908-xro77XaQOVY2gkU7H8cflBfYA6tV1wcCMqHU7U5',
+    access_token_secret:'I9C4HRA1fhVYHGdGoKNKFdJ1NQYvSvqKHnLchDrtGyVP3'
+};
